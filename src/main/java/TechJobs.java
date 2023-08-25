bc46dcc8f7e826ee644fe2e9cfa3ca0e2a930902
ssowers2 <sabriyasowers@gmail.com>
2023-08-25 15:40:51 -0500

    Intial Commit

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,19 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        for (int i = 0; i <someJobs.size(); i++) {
+            HashMap<String, String> job = someJobs.get(i);
+            System.out.println("*****");
+            System.out.println("position type: " + job.get("position type"));
+            System.out.println("name: " + job.get("name"));
+            System.out.println("employer: " + job.get("employer"));
+            System.out.println("location: " + job.get("location"));
+            System.out.println("core competency: " + job.get("core competency"));
+            System.out.println("*****");
+            System.out.println();
+        }
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        }
     }
 }
